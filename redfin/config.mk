2356580af308ba28f5905de5816febc4996e96ed
Alessandro Astone <ales.astone@gmail.com>
2021-06-20 19:44:19 +0200

    Initial commit

---
+++ redfin/config.mk
@@ -0,0 +1,15 @@
+FIRMWARE_IMAGES := \
+    abl \
+    aop \
+    devcfg \
+    featenabler \
+    hyp \
+    keymaster \
+    modem \
+    qupfw \
+    tz \
+    uefisecapp \
+    xbl \
+    xbl_config
+
+AB_OTA_PARTITIONS += $(FIRMWARE_IMAGES)
